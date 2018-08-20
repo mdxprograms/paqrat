9b16528f0377af9c899563dc978a20fcc59fcf91
Josh Waller <mdx.programs@gmail.com>
2018-08-20 00:25:54 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
-from lib.configure import get_templates_folder, get_content, get_data
+import lib.configure as paqrat
 
-app = Flask(__name__, template_folder=get_templates_folder())
-
+app = Flask(__name__, template_folder=paqrat.get_templates_folder())
+data = paqrat.get_data()
 
 @app.route("/")
 def index():
     return render_template("index.html",
-                           content=get_content(["index"]),
-                           data=get_data())
+                           content=paqrat.get_content(["index"]),
+                           data=data)
 
 
 @app.route("/about/")
 def about():
     return render_template("about.html",
-                           content=get_content(["about"]),
-                           data=get_data())
+                           content=paqrat.get_content(["about"]),
+                           data=data)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html", data=get_data()), 404
+    return render_template("404.html", data=data), 404
